04569d924ccce0c59f626a15d07c314d0090079e
greg2705 <gregoire2705@gmail.com>
2021-06-11 16:53:15 +0200

    Premier fonction

--- Interface/temp.py
+++ Interface/temp.py
@@ -4,22 +4,31 @@ from splinter import Browser
 from selenium import webdriver
 import time
 
+username="Cortex91Drahh"
+password="password"
+
+def login(username,password): #Cree un browser qui se connecte au site Showdown et se connecte au compte username password
+
+    home = os.path.expanduser("~")
+    path = {'executable_path': home + '/bin/chromedriver'} #Import avoir chromedriver dans ce dossier et l'avoir rendu executable
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized") #Affiche la fenetre en grand
+    options.add_argument("--disable-notifications") #Desactive les notifications chrome
+
+    browser = Browser('chrome', **path, headless=False, options=options) #Crée le Browser
+
+    browser.visit('https://play.pokemonshowdown.com/') #On se connecte au site showdown
+
+    browser.find_by_tag('button[name="login"]').click() #Puis on manipule la page HTML avec des methodes scraping pour se connecter
+    browser.find_by_tag('input[class="textbox autofocus"]').fill(username)
+    browser.find_by_tag('button[type="submit"]').click()
+    browser.find_by_tag('input[type="password"]').fill(password)
+    browser.find_by_tag('button[type="submit"]').click()
+
+    return browser #On return le browser pour continuer à l'utiliser
 
 
-home = os.path.expanduser("~")
-
-if(os.path.exists(home+'/bin/chromedriver')==False):
-    hah=0
-
-path = {'executable_path':home+'/bin/chromedriver'}
-
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-options.add_argument("--disable-notifications")
-
-browser = Browser('chrome',**path,headless=False, options=options)
-
-browser.visit('https://play.pokemonshowdown.com/')
-
+browser=login(username,password)
 time.sleep(10)
 browser.quit()
