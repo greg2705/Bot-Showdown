7865c4ee0426c11d3c1e0ef989a586cbc66a7fa9
greg2705 <gregoire2705@gmail.com>
2021-06-15 00:58:45 +0200

    Ca avance

--- Interface/play.py
+++ Interface/play.py
@@ -1,16 +1,10 @@
 # Interface
 import os.path
-
-
+from bs4 import BeautifulSoup
 from splinter import Browser
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import pyautogui as pag
-import time
 
-username = "Cortex91Drahh"
-password = "password"
+import time
 
 
 def login(username,
@@ -35,11 +29,21 @@ def login(username,
     browser.find_by_tag('input[type="password"]').fill(password)
     browser.find_by_tag('button[type="submit"]').click()
 
-    browser.find_by_tag('button[title="Sound"]').click() #on mute l'horrible son du jeu
+    browser.find_by_tag('button[title="Sound"]').click()  # on mute l'horrible son du jeu
     browser.find_by_tag('input[name="muted"]').click()
 
-
     return browser  # On return le browser pour continuer à l'utiliser
+
+def launch_game(browser,value):
+    browser.find_by_tag('button[class="select formatselect"]').click()
+    browser.find_by_tag('button[value="'+value+'"]').click()
+    browser.find_by_tag('button[class="button mainmenu1 big"]').click()
+    while(True):
+        time.sleep(1)
+        if(browser.is_element_present_by_tag('button[class="button timerbutton"]')):
+            browser.find_by_tag('button[class="button timerbutton"]').click()
+            browser.find_by_tag('button[name="timerOn"]').click()
+            return browser
 
 
 def move(browser, move):
@@ -82,6 +86,25 @@ def move(browser, move):
     return browser
 
 
-browser = login(username, password)
-time.sleep(10)
-browser.quit()
+def partie(browser):
+    print("debut")
+    endgame = False
+    numberturn = 0
+    while (endgame == False):
+        html = BeautifulSoup(browser.html, 'html.parser')
+        if (browser.is_element_present_by_tag('button[name="instantReplay"]')):
+            endgame = True
+        if(int(len(html.findAll("h2",class_='battle-history'))>numberturn)):
+            numberturn+=1
+            choix=str(input("Entrez votre attaque : "))
+            move(browser,choix)
+        time.sleep(2)
+    print("Partie Finie")
+    browser.quit()
+
+
+browser = login("Cortex91Drahh", "password")
+time.sleep(5)
+launch_game(browser,"gen4randombattle")
+
+
