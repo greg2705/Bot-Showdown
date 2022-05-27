a30f8b1a3e532b9c21342991f00e692257c80e0a
greg2705 <gregoire2705@gmail.com>
2022-05-27 18:01:13 +0200

    Bot fini

--- Interface/play.py
+++ Interface/play.py
@@ -1,5 +1,7 @@
 # Interface
 import os.path
+import random
+
 from bs4 import BeautifulSoup
 from splinter import Browser
 from selenium import webdriver
@@ -7,8 +9,7 @@ from selenium import webdriver
 import time
 
 
-def login(username,
-          password):  # Cree un browser qui se connecte au site Showdown et se connecte au compte username password
+def login(username,password):  # Cree un browser qui se connecte au site Showdown et se connecte au compte username password
 
     home = os.path.expanduser("~")
     path = {
@@ -22,8 +23,9 @@ def login(username,
 
     browser.visit('https://play.pokemonshowdown.com/')  # On se connecte au site showdown
 
-    browser.find_by_tag(
-        'button[name="login"]').click()  # Puis on manipule la page HTML avec des methodes scraping pour se connecter
+    browser.find_by_tag('button[class=" css-1wrbm"]').click()
+
+    browser.find_by_tag('button[name="login"]').click()  # Puis on manipule la page HTML avec des methodes scraping pour se connecter
     browser.find_by_tag('input[class="textbox autofocus"]').fill(username)
     browser.find_by_tag('button[type="submit"]').click()
     browser.find_by_tag('input[type="password"]').fill(password)
@@ -34,77 +36,65 @@ def login(username,
 
     return browser  # On return le browser pour continuer à l'utiliser
 
-def launch_game(browser,value):
+def launch_game(browser,value): #Lance une game à partir d'un browser , value permet de choisir le mode de jeu
     browser.find_by_tag('button[class="select formatselect"]').click()
     browser.find_by_tag('button[value="'+value+'"]').click()
-    browser.find_by_tag('button[class="button mainmenu1 big"]').click()
-    while(True):
+    browser.find_by_tag('button[class="button mainmenu1 big"]').click() #On choisit le mode de jeu et on lance la partie
+    while(True): #Permet de detecter quand une partie est lancé
         time.sleep(1)
-        if(browser.is_element_present_by_tag('button[class="button timerbutton"]')):
+        if(browser.is_element_present_by_tag('button[class="button timerbutton"]')): #On lance le timer
             browser.find_by_tag('button[class="button timerbutton"]').click()
             browser.find_by_tag('button[name="timerOn"]').click()
             return browser
 
 
-def move(browser, move):
-    if move == "m1":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/move 1\n")
-        return browser
-
-    elif move == "m2":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/move 2\n")
-        return browser
-
-    elif move == "m3":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/move 3\n")
-        return browser
-
-    elif move == "m4":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/move 4\n")
-        return browser
-
-    elif move == "s2":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/switch 2\n")
-        return browser
-
-    elif move == "s3":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/switch 3\n")
-        return browser
-
-    elif move == "s4":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/switch 4\n")
-        return browser
-
-    elif move == "s5":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/switch 5\n")
-        return browser
-
-    elif move == "s6":
-        browser.find_by_tag('textarea[class="textbox"]').last.fill("/switch 6\n")
-        return browser
-
-    return browser
-
-
-def partie(browser):
-    print("debut")
+def partie(browser): #Permet le déroulement d'une partie
     endgame = False
-    numberturn = 0
-    while (endgame == False):
+    nbalive=6
+    numberturn = 0 #On coute le nombre de tour pour savoir qaund il faut jouer
+    while (endgame == False): #Boucle tant que la partie n'est pas fini
         html = BeautifulSoup(browser.html, 'html.parser')
-        if (browser.is_element_present_by_tag('button[name="instantReplay"]')):
+        if (browser.is_element_present_by_tag('button[name="instantReplay"]')): #On regarde si la partie est finie
             endgame = True
-        if(int(len(html.findAll("h2",class_='battle-history'))>numberturn)):
-            numberturn+=1
-            choix=str(input("Entrez votre attaque : "))
-            move(browser,choix)
-        time.sleep(2)
-    print("Partie Finie")
-    browser.quit()
+        if(len(get_list_move(browser))>0):#Permet de savoir si on doit faire un move
+            make_move(browser)
+        time.sleep(1)
 
+    browser.quit() #On quitte le jeu quand la partie est finie
 
-browser = login("Cortex91Drahh", "password")
+def get_list_move(browser):
+    if(browser.is_element_present_by_tag('button[name="chooseMove"]')):
+        moov=browser.find_by_tag('button[name="chooseMove"]')
+        moove=["m"+str(m.value) for m in moov]
+    else:
+        moove=[]
+    if(browser.is_element_present_by_tag('button[name="chooseSwitch"]')):
+        switc=browser.find_by_tag('button[name="chooseSwitch"]')
+        switch=["s" + str(s.value) for s in switc]
+    else:
+        switch=[]
+    return moove+switch
+
+def make_move(browser):
+    if(browser.is_element_present_by_tag('button[name="chooseMove"]')):
+        moov=browser.find_by_tag('button[name="chooseMove"]')
+        moove=["m"+str(m.value) for m in moov]
+    else:
+        moove=[]
+    if(browser.is_element_present_by_tag('button[name="chooseSwitch"]')):
+        switc=browser.find_by_tag('button[name="chooseSwitch"]')
+        switch=["s" + str(s.value) for s in switc]
+    else:
+        switch=[]
+    radn=random.choice(range(len(moove+switch)))
+    if(radn<len(moove)):
+        moov[radn].click()
+    else:
+        switc[radn-len(moove)].click()
+    return
+
+browser = login("Botshow","projetpython")
 time.sleep(5)
 launch_game(browser,"gen4randombattle")
-
+partie(browser)cd
 
